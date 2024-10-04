f987bd85c37ed74426984412b70cf7e08b26e31f
Mikhail Titov <mlt@gmx.us>
2024-10-04 10:57:35 -0500

    Clarify that C code is under BSL-1.0 and not GPLv3
    - It is meant to be a reference only
    - It can be compiled and linked in as is
    - No warranty
    - No attribution required

--- get_message.c
+++ get_message.c
@@ -1,3 +1,9 @@
+/*
+Copyright Mikhail Titov 2024.
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+*/
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdlib.h>
